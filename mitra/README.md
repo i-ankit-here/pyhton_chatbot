c0cc420331111834ee34b34dbb21de294b7573e8
ANKIT <closetooankit@gmail.com>
2024-04-29 15:43:39 +0530

    option based chatbot prepared

--- mitra/README.md
+++ mitra/README.md
@@ -1,72 +1,11 @@
-![Binaryhood](Logo/BinaryhoodLogo.png)
+1. Scraping website
+We have used "scrapy" framework of python for scraping NIT Jalandhar website. We have used a spider for crawling through the website to get all anchor tags their href attribute and text. We have used another spider to scrape out paragraph content from the anchor tags scraped out previously. This has been stored in a json file which contains text as title, href as url and paragraph as content.
 
-# ChatBot
+2. Creating the Bot
+a. Data Preprocessing: Used NLP preprocessing techniques such as tokenization, lowercasing, punctuation removal, stop word removal, and lemmatization to clean and normalize textual data extracted from the JSON file containing the information.
+b. Intent Classification: Trained a machine learning model, Support Vector Machine (SVM), using TF-IDF vectorization, to classify user queries into predefined intents based on the content in the json file. The parameters supplied to the pipeline were processed using NLTK processing first.
+c. Response Generation: Created a logic to generate response from the chatbot based on the user input. A dictionary was created to store different domains and subdomains of queries user can choose from. The user input is passed to the pipeline created before to predict intent. On basis of the user input and the intent predicted a response is generated which would be given by the bot.
 
-## Installation & Setup
+3. Serving the bot
+We have used Flask framework for integrating our bot with the server.
 
-[Install Python] https://www.dataquest.io/blog/installing-python-on-mac/
-
-[Install pip] https://phoenixnap.com/kb/install-pip-mac
-
-If you have Python & pip installed then check their version in the terminal or command line tools
-
-```
-python3 --version
-```
-
-```
-pip --version
-```
-
-## Installing Flask
-
-In your terminal run the requirements.txt file using this pip
-
-```
-pip install -r requirements.txt
-```
-
-
-## Running ChatBot Application in Terminal
-
-```
-cd into your directory
-```
-
-```
-python app.py
-```
-
-
-
-## What you will create
-
-In this tutorial, I will guide you through the process of building a chatbot that can carry out conversations with users using natural language processing.
-
-To start, we will be using Microsoft DialoGPT, a pre-trained language model that can generate human-like responses to given prompts. We will be integrating DialoGPT with Flask, a popular Python web framework, to create a web application that can communicate with users via a chat interface.
-
-For the frontend of our application, we will be using HTML, CSS, and JavaScript to create a visually appealing and interactive chat interface. Additionally, we will be using jQuery to handle the HTTP requests that are made to the backend server.
-
-Throughout the tutorial, I will provide step-by-step instructions on how to set up your development environment, install the necessary dependencies, and create the required files and code for the application. I will also explain how to train and fine-tune the DialoGPT model to improve the accuracy of its responses.
-
-By the end of this tutorial, you will have a fully functional chatbot that can engage in conversations with users, and you will have gained valuable experience in using Microsoft DialoGPT, Flask, and web development technologies such as HTML, CSS, and JavaScript.
-
-# ChatBot Link
-The Chatbot is constructed using the Microsoft/DialoGPT-medium model.
-
-```
-https://huggingface.co/microsoft/DialoGPT-medium
-```
-
-# User-Html
-
-```
-var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + user_input + '<span class="msg_time_send">'+ time + 
-    '</span></div><div class="img_cont_msg"><img src="https://i.ibb.co/d5b84Xw/Untitled-design.png" class="rounded-circle user_img_msg"></div></div>';
-```
-
-# Bot-HTML
-
-```
-var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + bot_response + '<span class="msg_time">' + time + '</span></div></div>';
-```
--- mitra/app.py
+++ mitra/app.py
@@ -1,3 +1,4 @@
+# IMPORTING REQUIRED LIBRARIES AND MODULES
 from flask import Flask, render_template, request, jsonify
 import json
 from nltk.tokenize import word_tokenize
@@ -8,32 +9,23 @@ from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 import string
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
+#ACCESSING INFORMATION STORED IN JSON FILE
 with open('intents.json', 'r', encoding="utf8") as file:
     intents_data = json.load(file)
 
+
+#FUNCTIONS TO PROCESS VARIOUS TEXTS
+
 def preprocess_text(text):
-    # Tokenization
+    # TOKENIZATION, LOWERCASING, REMOVING PUNCTUATION, REMOVING STOPWORDS, LEMMATIZATION
     tokens = word_tokenize(text)
-    
-    # Lowercasing
     tokens = [token.lower() for token in tokens]
-    
-    # Removing punctuation
-    tokens = [token for token in tokens if token not in string.punctuation]
-    
-    # Removing stopwords
     stop_words = set(stopwords.words('english'))
     tokens = [token for token in tokens if token not in stop_words]
-    
-    # Lemmatization
     lemmatizer = WordNetLemmatizer()
     tokens = [lemmatizer.lemmatize(token) for token in tokens]
-    
     return " ".join(tokens)
 
-# Preprocess intents
 def process_str(s):
     tokens= word_tokenize(s)
     return " ".join(tokens)
@@ -46,9 +38,11 @@ def process_content(intent):
             content_string=content_string+'\n'+str+'\n'
     return content_string
 
+#PREPARING INPUTS FOR PIPELINE
 texts = [preprocess_text(process_content(intent) + intent['url']) for intent in intents_data['intents']]
 labels = [intent["title"] for intent in intents_data['intents']]
 
+#PREPARING PIPELINE FOR TRAINING
 vectorizer = TfidfVectorizer()
 classifier = SVC(kernel='linear')
 pipeline = Pipeline([
@@ -56,11 +50,55 @@ pipeline = Pipeline([
     ('clf', classifier)
 ])
 
+#EXECUTING PIPELINE FOR INTENT PREDICTION
 pipeline.fit(texts, labels)
 def predict_intent(query):
     query_processed = preprocess_text(query)
     intent = pipeline.predict([query_processed])
     return intent[0]
+
+#DEFINING DOMAINS AND SUBDOMAINS
+About_NITJ=["Why NITJ","Reach Us","Institute Flyers","Organisational Chart","NITJ Rankings & Awards","Virtual Tour","Newsletters","General FAQs","Other"]
+Administration=["NIT Council","Hostel Administration","Senate","Finance Committee","Building and Works Committee","System of Evaluation and Grant of Division","Other"]
+Leadership=["Chairperson","Registrar","Administrative Officers","Board of Governors","Technical Officers","Other"]
+Cells=["RTI Cell","Equal Opportunities and SC/ST/OBC/PwD Cell","Women Cell","Proctorial Cell","Student Grievances Cell","Public Grievances Cell","Staff Grievances Cell","Other"]
+Departments=["Computer Science and Engineering","Information Technology","Instrumentation and Control Engineering","Industrial and Production Engineering","Electrical Engineering","Physics","Electronics and Communication Engineering","Civil Engineering","Chemical Engineering","Biotechnology","Chemistry","Humanities and Management","Mathematics and Computing","Mechanical Engineering","Textile Technology","Center for Energy and Environment","Other"]
+Academics=["UMC Rules","Academic Circulars & Notices","Grade Calculation","Student Portal","Online Application for Verification of Degree","Results","CGPA Criteria and Medium of Education","Academic Section Officials","Other"]
+Admissions=["Bachelor of Technology","Master of Technology","Master of Business Administration","Master of Science","Post Graduate Diploma","Doctor of Philosophy","International Students","Anti Ragging Cell","National anti-Ragging Programme","Other"]
+Alumni=["Convocation","Past Convocations","Other"]
+Life_at_NITJ=["Discover Student Life","Explore NITJ","Other"]
+headers={'About NITJ':About_NITJ,'Administration':Administration,'Leadership':Leadership,'Cells':Cells,'Departments':Departments,'Academics':Academics,'Admissions':Admissions,'Alumni':Alumni,'Life at NITJ':Life_at_NITJ}
+
+#FUNCTIONS TO PRODUCE BOT RESPONSE
+def get_Chat_response(text1):
+        if process_chat(text1)=="" or process_chat(text1) == "none":
+            response=["For information about NITJ, please visit","<a href='https://www.nitj.ac.in'>https://www.nitj.ac.in</a>"]
+            return response
+        elif process_chat(text1)=="other":
+            response=["Please specify"]
+            return response
+        else:
+            key=''
+            for i in headers.keys():
+                if process_chat(text1)==process_chat(i):
+                    key+=i
+            if key:
+                response = headers[key] 
+                return response
+            elif process_chat(text1)=="" or process_chat(text1)=="none":
+                response=["For information about NITJ, please visit","<a href='https://www.nitj.ac.in'>https://www.nitj.ac.in</a>"]
+            elif process_chat(text1)=="other":
+                response=["Please specify"]
+                return response
+            else:
+                intent = predict_intent(text1)
+                if intent:
+                    response = generate_response(intent)
+                else:
+                    response = "Sorry, I'm not sure how to help with that."
+        if process_chat(text1)=='exit':
+            pass
+        return response
 
 def generate_response(intent):
     for intent_data in intents_data['intents']:
@@ -74,27 +112,14 @@ def generate_response(intent):
 
 def process_chat(s):
     s=s.lower().strip()
-    # print(s)
     return s
 
-About_NITJ=["Why NITJ","Reach Us","Institute Flyers","Organisational Chart","NITJ Rankings & Awards","Virtual Tour","Newsletters","General FAQs"]
-Administration=["NIT Council","Hostel Administration","Senate","Finance Committee","Building and Works Committee","System of Evaluation and Grant of Division"]
-Leadership=["Chairperson","Registrar","Administrative Officers","Board of Governors","Technical Officers"]
-Cells=["RTI Cell","Equal Opportunities and SC/ST/OBC/PwD Cell","Women Cell","Proctorial Cell","Student Grievances Cell","Public Grievances Cell","Staff Grievances Cell"]
-Departments=["Computer Science and Engineering","Information Technology","Instrumentation and Control Engineering","Industrial and Production Engineering","Electrical Engineering","Physics","Electronics and Communication Engineering","Civil Engineering","Chemical Engineering","Biotechnology","Chemistry","Humanities and Management","Mathematics and Computing","Mechanical Engineering","Textile Technology","Center for Energy and Environment"]
-Academics=["UMC Rules","Academic Circulars & Notices","Grade Calculation","Student Portal","Online Application for Verification of Degree","Results","CGPA Criteria and Medium of Education","Academic Section Officials",]
-Admissions=["Bachelor of Technology","Master of Technology","Master of Business Administration","Master of Science","Post Graduate Diploma","Doctor of Philosophy","International Students","Anti Ragging Cell","National anti-Ragging Programme",]
-Alumni=["Convocation","Past Convocations"]
-Life_at_NITJ=["Discover Student Life","Explore NITJ"]
-headers={'About NITJ':About_NITJ,'Administration':Administration,'Leadership':Leadership,'Cells':Cells,'Departments':Departments,'Academics':Academics,'Admissions':Admissions,'Alumni':Alumni,'Life at NITJ':Life_at_NITJ}
-
-
+#SERVING BOT USING FLASK
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('chat.html')
-
+    return render_template('index.html')
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
@@ -102,29 +127,5 @@ def chat():
     input = msg
     return get_Chat_response(input)
 
-
-def get_Chat_response(text1):
-        if process_chat(text1)=="" or process_chat(text1) == "none":
-            response=["For information about NITJ, please visit","<a href='https://www.nitj.ac.in'>https://www.nitj.ac.in</a>"]
-            return response
-        else:
-            key=''
-            for i in headers.keys():
-                if process_chat(text1)==process_chat(i):
-                    key+=i
-            if key:
-                response = headers[key] 
-                return response
-            elif process_chat(text1)=="" or process_chat(text1)=="none":
-                response=["For information about NITJ, please visit","<a href='https://www.nitj.ac.in'>https://www.nitj.ac.in</a>"]
-            else:
-                intent = predict_intent(text1)
-                if intent:
-                    response = generate_response(intent)
-                else:
-                    response = "Sorry, I'm not sure how to help with that."
-        if process_chat(text1)=='exit':
-            pass
-        return response
 if __name__ == '__main__':
     app.run()
